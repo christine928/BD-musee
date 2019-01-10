e389fbee72db9e53a4b2dd8abe9871021264f5ea
OceaneFlam <34216308+OceaneFlam@users.noreply.github.com>
2019-01-10 15:31:27 +0100

    telephone varchar(12)
    car si on rentre +33... il faut 12 caractères c'est juste après qu'on modifiera nous même

--- creat_table.sql
+++ creat_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Particuliers( IDparticulier int CONSTRAINT pk_particuliers PRIMARY KEY, nom varchar(25), adresse varchar(50), telephone varchar(10) );
+CREATE TABLE Particuliers( IDparticulier int CONSTRAINT pk_particuliers PRIMARY KEY, nom varchar(25), adresse varchar(50), telephone varchar(12) );
 
  
 
