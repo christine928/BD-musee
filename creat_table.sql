3e38aa068c715a72b1a42db648632f483a55cba7
OceaneFlam <34216308+OceaneFlam@users.noreply.github.com>
2019-01-10 16:52:30 +0100

    Update creat_table.sql

--- creat_table.sql
+++ creat_table.sql
@@ -1,4 +1,1 @@
-CREATE TABLE Particuliers( IDparticulier int CONSTRAINT pk_particuliers PRIMARY KEY, nom varchar(25), adresse varchar(50), telephone number(10) );
-
- 
-
+CREATE TABLE Particuliers( IDparticulier int CONSTRAINT pk_particuliers PRIMARY KEY, nom varchar(25), adresse varchar(50), telephone varchar(12) );
