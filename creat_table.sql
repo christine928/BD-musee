b8b5f41640186a28b84fc0b35d088ac379ddeb8b
OceaneFlam <34216308+OceaneFlam@users.noreply.github.com>
2019-01-10 16:29:48 +0100

    telephone number(10)

--- creat_table.sql
+++ creat_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Particuliers( IDparticulier int CONSTRAINT pk_particuliers PRIMARY KEY, nom varchar(25), adresse varchar(50), telephone varchar(12) );
+CREATE TABLE Particuliers( IDparticulier int CONSTRAINT pk_particuliers PRIMARY KEY, nom varchar(25), adresse varchar(50), telephone number(10) );
 
  
 
